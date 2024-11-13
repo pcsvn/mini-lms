7742e7c86d201a1a074cef7a1d4dfe425c79dd6a
JaxVN <hongbaongan@gmail.com>
2024-11-13 07:30:50 +0000

    Initial commit for LMS Docker project

---
+++ lms/courses/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
---
+++ lms/lms/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lms_db',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
